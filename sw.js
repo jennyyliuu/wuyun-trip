2f27024e80aece48e42825c88a2c2d97428016e4
jennyyliuu <jenny81952@gmail.com>
2026-09-22 10:29:16 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const C='wuyun-v1',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='wuyun-v2';
+const A=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),...caches.keys().then(keys=>keys.filter(k=>k!==C).map(k=>caches.delete(k)))])));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
